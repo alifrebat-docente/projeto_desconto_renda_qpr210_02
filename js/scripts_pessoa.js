db505593689bdb4c25e59d42eb012072fabc0416
Alisson Freire <alisson.batista@docente.senai.br>
2026-06-19 18:38:21 -0300

    Criado o array pessoas pego os elementos do dom

---
+++ js/scripts_pessoa.js
@@ -0,0 +1,9 @@
+
+//CRIAR UM ARRAY DE OBJETOS PESSOA
+const pessoas = []
+
+//PEGANDO ELEMENTOS DO DOM
+const formPessoa = document.querySelector('#div-form')
+const divPessoa = document.querySelector('#div-lista-pessoas')
+
+//CAPTURANDO O EVENTO DO SUBMIT DO FORMULÁRIO
